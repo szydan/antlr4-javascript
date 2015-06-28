225e162ad3163291e2106228b486cbd2a1c3fb16
Szymon Danielczyk <danielczyk.szymon@gmail.com>
2015-06-28 08:55:52 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+antlr4-javascript runtime as UMD
+================================
+
+## Why
+I had to be able to load antlr javascript runtime in browser as AMD 
---
+++ main.js
@@ -0,0 +1,17 @@
+var antlr4 = require('antlr4'); 
+
+(function (root, factory) {
+
+    if (typeof define === 'function' && define.amd) {
+        // AMD
+        define([], factory);
+    } else if (typeof exports === 'object') {
+        // Node, CommonJS-like
+        module.exports = factory();
+    } else {
+        // Browser globals (root is window)
+        root.returnExports = factory();
+    }
+}(this, function () {
+    return antlr4;
+}));
